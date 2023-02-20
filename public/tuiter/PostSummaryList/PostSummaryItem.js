63492f4cbe4aef97277ec63707f119b841ef0f69
bkornusov <batyrkornusov@gmail.com>
2023-02-19 23:59:22 -0500

    minor

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,13 +1,8 @@
 function PostSummaryItem(post) {
   let listItem = `
     <li class="list-group-item pt-2 ms-0 me-0 row" id="inline">
-      <div class="col-10">
+      <div class="col-10" >
         <span class="wd-trend-subtitle">${post.topic}</span><br />
-        <span class="wd-trend-title">${post.userName}</span>
-        <span class="fa-stack">
-          <i class="fas fa-certificate fa-stack-2x"></i>
-          <i class="fas fa-check fa-stack-1x fa-inverse"></i>
-        </span>
         <span class="wd-trend-subtitle"> - ${post.time}</span><br />
         <p class="wd-trend-title">
           ${post.title}
@@ -27,6 +22,19 @@ function PostSummaryItem(post) {
       splitItem[0] +
       `<span class="wd-trend-subtitle">${post.tweets} Tweets</span>` +
       splitItem[1];
+  }
+  if (post.userName) {
+    let splitITem = listItem.split(
+      `<span class="wd-trend-subtitle">${post.topic}</span><br />`
+    );
+    listItem =
+      splitITem[0] +
+      `<span class="wd-trend-title" id="no-wrap">${post.userName}</span>` +
+      `<span class="fa-stack">
+          <i class="fas fa-certificate fa-stack-2x"></i>
+          <i class="fas fa-check fa-stack-1x fa-inverse"></i>
+        </span>` +
+      splitITem[1];
   }
   return listItem;
 }
